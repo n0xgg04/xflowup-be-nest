879e6fe378638631d22bcaee45b76cd28a49aee7
Luong Tuan Anh <dulieu.vblc@gmail.com>
2025-03-23 16:24:31 +0700

    chore: update Apollo Server plugin configuration to use local landing page and modify redirect URL in AuthController

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -29,9 +29,11 @@ export class AuthController {
 
       const { accessToken } = await this.authService.login(user);
 
-      return res.redirect(
-        `${this.configService.get('FRONTEND_URL', 'http://localhost:3000')}/auth/callback?access_token=${accessToken}`,
-      );
+      return res.send(`
+        <script>
+          window.location.href = "${this.configService.get('FRONTEND_URL', 'http://localhost:3000')}/auth/callback?access_token=${accessToken}";
+        </script>
+      `);
     } catch (error) {
       return res.redirect(
         `${this.configService.get('FRONTEND_URL', 'http://localhost:3000')}/auth/failed`,
